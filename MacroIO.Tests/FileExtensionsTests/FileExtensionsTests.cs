eac09f604ed16ae5c0b1188f854b42ab139ed54b
Ron MacNeil <macro@hotmail.com>
2019-11-17 12:39:59 +1000

    Add StartsWithUtf8BomTests

---
+++ MacroIO.Tests/FileExtensionsTests/FileExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MacroSystem;
+
+
+namespace
+MacroIO.Tests.FileExtensionsTests
+{
+
+
+[TestClass]
+public class
+DetectLineEndingsTests
+{
+
+
+const string NoLineEndings =    "...";
+const string CRLFOnly =         "\x0D\x0A";
+const string CROnly =           "\x0D";
+const string LFOnly =           "\x0A";
+const string CRLFAtStart =      "\x0D\x0A...";
+const string CRAtStart =        "\x0D...";
+const string LFAtStart =        "\x0A...";
+const string CRLFInMiddle =     "...\x0D\x0A...";
+const string CRInMiddle =       "...\x0D...";
+const string LFInMiddle =       "...\x0A...";
+const string CRLFAtEnd =        "...\x0D\x0A";
+const string CRAtEnd =          "...\x0D";
+const string LFAtEnd =          "...\x0A";
+
+
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void
+Null_throws_ArgumentNullException()
+{
+    FileExtensions.DetectLineEndings((TextReader)null);
+}
+
+
+[TestMethod]
+public void
+No_line_endings_returns_null()
+{
+    DetectLineEndings(null, NoLineEndings);
+}
+
+
+[TestMethod] public void CRLF_only_is_correct() { DetectLineEndings(LineEnding.CRLF, CRLFOnly); }
+[TestMethod] public void CR_only_is_correct() { DetectLineEndings(LineEnding.CR, CROnly); }
+[TestMethod] public void LF_only_is_correct() { DetectLineEndings(LineEnding.LF, LFOnly); }
+[TestMethod] public void CRLF_at_start_is_correct() { DetectLineEndings(LineEnding.CRLF, CRLFAtStart); }
+[TestMethod] public void CR_at_start_is_correct() { DetectLineEndings(LineEnding.CR, CRAtStart); }
+[TestMethod] public void LF_at_start_is_correct() { DetectLineEndings(LineEnding.LF, LFAtStart); }
+[TestMethod] public void CRLF_in_middle_is_correct() { DetectLineEndings(LineEnding.CRLF, CRLFInMiddle); }
+[TestMethod] public void CR_in_middle_is_correct() { DetectLineEndings(LineEnding.CR, CRInMiddle); }
+[TestMethod] public void LF_in_middle_is_correct() { DetectLineEndings(LineEnding.LF, LFInMiddle); }
+[TestMethod] public void CRLF_at_end_is_correct() { DetectLineEndings(LineEnding.CRLF, CRLFAtEnd); }
+[TestMethod] public void CR_at_end_is_correct() { DetectLineEndings(LineEnding.CR, CRAtEnd); }
+[TestMethod] public void LF_at_end_is_correct() { DetectLineEndings(LineEnding.LF, LFAtEnd); }
+
+
+void
+DetectLineEndings(LineEnding expected, string s)
+{
+    using (var text = new StringReader(s)) Assert.AreEqual(expected, FileExtensions.DetectLineEndings(text));
+}
+
+
+}
+}
---
+++ MacroIO.Tests/FileExtensionsTests/StartsWithUtf8BomTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace
+MacroIO.Tests.FileExtensionsTests
+{
+
+
+[TestClass]
+public class
+StartsWithUtf8BomTests
+{
+
+
+static readonly byte[] Bom =                new[] { (byte)0xEF, (byte)0xBB, (byte)0xBF, };
+static readonly byte[] PartialBom =         new[] { (byte)0xEF, (byte)0xBB, };
+static readonly byte[] OtherBytes =         new[] { (byte)0x00, (byte)0x11, (byte)0x22, };
+static readonly byte[] BomThenOtherBytes =  new[] { (byte)0xEF, (byte)0xBB, (byte)0xBF, (byte)0x00, (byte)0x11, (byte)0x22, };
+static readonly byte[] OtherBytesThenBom =  new[] { (byte)0x00, (byte)0x11, (byte)0x22, (byte)0xEF, (byte)0xBB, (byte)0xBF, };
+
+
+[TestMethod]
+[ExpectedException(typeof(ArgumentNullException))]
+public void
+Null_stream_throws_ArgumentNullException()
+{
+    FileExtensions.StartsWithUtf8Bom((Stream)null);
+}
+
+
+[TestMethod]
+public void
+Bom_by_itself_is_true()
+{
+    StartsWithUtf8Bom(true, Bom);
+}
+
+
+[TestMethod]
+public void
+Bom_then_other_bytes_is_true()
+{
+    StartsWithUtf8Bom(true, BomThenOtherBytes);
+}
+
+
+[TestMethod]
+public void
+Partial_Bom_is_false()
+{
+    StartsWithUtf8Bom(false, PartialBom);
+}
+
+
+[TestMethod]
+public void
+Not_Bom_is_false()
+{
+    StartsWithUtf8Bom(false, OtherBytes);
+}
+
+
+[TestMethod]
+public void
+Bom_preceded_by_other_bytes_is_false()
+{
+    StartsWithUtf8Bom(false, OtherBytesThenBom);
+}
+
+
+void
+StartsWithUtf8Bom(bool expected, params byte[] bytes)
+{
+    var stream = new MemoryStream(bytes);
+    Assert.AreEqual(expected, FileExtensions.StartsWithUtf8Bom(stream));
+}
+
+
+}
+}
